998f50015e12813baf0e9777d25316ca7657c9a0
Jorge Lopez <jorge.lopez.18820@gmail.com>
2013-11-13 00:02:15 -0800

    Agregado algunas clases

---
+++ Launch/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Launch
+{
+    class Cliente : Usuario
+    {
+        //List<Aplicaciones> appsInstaladas = new List<Aplicaciones>();
+
+        public Cliente(string Nombre, string Correo)
+        {
+            this.Nombre = Nombre;
+            this.Correo = Correo;
+        }
+
+        public void Instalar()
+        { }
+        public void ComprarMembresia()
+        { }
+        public void Buscar()
+        { }
+        public void Remover()
+        { }
+    }
+}
---
+++ Launch/Comentarios.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Launch
+{
+    class Comentarios
+    {
+        public string Usuario { get; private set; }
+        public string Correo{ get; private set; }
+        public DateTime Fecha { get; private set; }
+        public string Contenido { get; private set; }
+
+        public Comentarios(Usuario Usuario, string Comentario)
+        {
+            this.Usuario = Usuario.Nombre;
+            this.Correo = Usuario.Correo;
+            this.Contenido = Comentario;
+            this.Fecha = DateTime.Today;
+        }
+        public Comentarios(string Usario, string Correo, string Comentario)
+        {
+            this.Usuario = Usuario;
+            this.Correo = Correo;
+            this.Contenido = Comentario;
+            this.Fecha = DateTime.Today;
+        }
+        
+    }
+}
