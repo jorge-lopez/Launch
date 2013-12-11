d02fc01fcbc499c6a5300bd3c3b6d5745bd3e178
Emmanuel.Gutierrez <emmanuel.gutierrez@cetys.edu.mx>
2013-12-11 15:04:20 -0800

    Metodos de Registro y Login parte 1
    Se creo una clase para pasar datos a la base de datos para el registro y
    login.

---
+++ Datos/Registro_Login.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Registro_Login
+    {
+        //Metodo que agrega un Costumer a la base de datos
+        public static bool AddCostumer(string _FirstName, string _LastName, string _Email, string _Password)
+        {
+            using (var dbContext= new LAUNCHEntities())
+            {
+                var costumer = new COSTUMER
+                {
+                    FirstName = _FirstName,
+                    LastName = _LastName,
+                    Email = _Email,
+                    Password = _Password
+                };
+                dbContext.COSTUMERs.Add(costumer);
+
+                var changesSaved = dbContext.SaveChanges();
+
+                return changesSaved >= 1;
+            }
+
+
+        }
+    }
+}
